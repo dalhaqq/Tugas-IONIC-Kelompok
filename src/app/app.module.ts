e55f67684704c612fd07a845ba623af989b1179b
Abdalhaqq Muhammad Saih <abdalhaqqm@gmail.com>
2022-11-18 06:39:46 +0700

    implementasi login

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -7,10 +7,13 @@ import { IonicModule, IonicRouteStrategy } from '@ionic/angular';
 import { AppComponent } from './app.component';
 import { AppRoutingModule } from './app-routing.module';
 
+import { HttpClientModule } from '@angular/common/http';
+import { Network } from '@ionic-native/network/ngx';
+
 @NgModule({
   declarations: [AppComponent],
-  imports: [BrowserModule, IonicModule.forRoot(), AppRoutingModule],
-  providers: [{ provide: RouteReuseStrategy, useClass: IonicRouteStrategy }],
+  imports: [BrowserModule, IonicModule.forRoot(), HttpClientModule, AppRoutingModule],
+  providers: [Network,{ provide: RouteReuseStrategy, useClass: IonicRouteStrategy }],
   bootstrap: [AppComponent],
 })
 export class AppModule {}
--- src/app/services/authentication.service.ts
+++ src/app/services/authentication.service.ts
@@ -1,9 +1,35 @@
 import { Injectable } from '@angular/core';
-
+import { HttpClient } from '@angular/common/http';
+import { BehaviorSubject } from 'rxjs';
+import { Preferences } from '@capacitor/preferences';
+const TOKEN_KEY = 'token-saya';
+const USERNAME = 'namasaya';
 @Injectable({
   providedIn: 'root'
 })
 export class AuthenticationService {
-
-  constructor() { }
+  // Inisialisasi is auth
+  isAuthenticated: BehaviorSubject<boolean|null> = new BehaviorSubject<boolean|null>(null);
+  token = '';
+  constructor(private http: HttpClient) {
+    this.loadToken();
+  }
+  async loadToken() {
+    const token = await Preferences.get({ key: TOKEN_KEY });
+    if (token && token.value) {
+      console.log('set token: ', token.value);
+      this.token = token.value;
+      this.isAuthenticated.next(true);
+    } else {
+      this.isAuthenticated.next(false);
+    }
+  }
+  apiURL() {
+    return "http://localhost:8080/";
+  }
+  logout(): Promise<void> {
+    this.isAuthenticated.next(false);
+    Preferences.remove({ key: USERNAME });
+    return Preferences.remove({ key: TOKEN_KEY });
+  }
 }
